af8c89e3885f146da97e9612128becef3d285b90
httpswwwmapexdotcom <21shukladh@hazelwick.org>
2026-09-25 12:19:08 +0000

    25.9.26

--- work.py/work.py
+++ work.py/work.py
@@ -1,13 +1,15 @@
 Fname = input("Enter your forename: ")
 Lname = input("Enter your surname: ")
 age = int(input("Enter your age: "))
-
+continuation = str(input("Do you want to continue? (y/n): "))
 answer = input("Do you want to print your name or username: ")
+
+
 if answer == "name":
- print (Fname.title(), Lname.title())
+  print (Fname.title(), Lname.title())
 elif answer == "username":
- print(Lname[0].title() + Fname[0:5].title() + age)
+  print(Lname[0].title() + Fname[0:5].title() + str(age))
 else:
- end()
+
 
 # add a loop function that will keep it running and ask if u want to continue
